749f29fdc5dd6d60e3ec9317ef32ea70233d1ec2
szarrinkelk <szarrinkelk@gmail.com>
2017-09-11 10:48:15 +0200

    added xcode project file containing the theplayer sdk framework

--- index.ios.js
+++ index.ios.js
@@ -7,25 +7,58 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ActivityIndicator,
+  ListView,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
 
+var API_URL = 'https://mychannels-api-preview.herokuapp.com/v1/productions'
+//'https://facebook.github.io/react-native/movies.json'
+
 export default class react_native_theoplayer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch(API_URL)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson.production),
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{flex: 1, paddingTop: 20}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.id},{rowData.releaseYear}</Text>}
+        />
       </View>
     );
   }
@@ -34,19 +67,17 @@ export default class react_native_theoplayer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
